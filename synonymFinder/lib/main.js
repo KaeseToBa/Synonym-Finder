9fd088622397a93ab8d228362f0126efe817594e
Simon Ritzel <ritzels@hochschule-trier.de>
2014-08-26 22:24:34 +0200

    working

--- synonymFinder/lib/main.js
+++ synonymFinder/lib/main.js
@@ -5,34 +5,31 @@ var word;
 
 var cntntPnl = pnl.Panel({
     contentURL: data.url("content.html"), 
-    contentScriptFile: [data.url("jquery-2.1.1.min.js"), data.url("scripts.js")]
 });
-
-var selContext = cm.SelectionContext();
-var selContentScript = 'self.on("context", function () {' +
-                    'var selText = window.getSelection().toString(); ' +
-                    '   if (selText != null && selText != "" && !selText.match("^\w*\S*\w$")) { ' +
-                    '       console.log("selText: " + selText); ' +
-                    '       self.postMessage(selText); ' +
-                    '   } ' +
-                    '});';
 
 var synFindItem = cm.Item({
   label: "Find Synonym",
+  
   context: cm.PredicateContext( function predicateFunction(context) {
       var selText = context.selectionText;
-      if (selText != null && selText != "" ){ //&& selText.match("^\w*\S*\w*$")){
-            synFindItem.context.add(selContext);
-            synFindItem.contentScript = selContentScript;
-            return true;
+      if (selText != null && selText != "" && !selText.match("^\w*\S*\w$")) {
+    	  console.log("selection predicate true");
+    	  return true;
       }
       return false;
   }),
+  
+  contentScript: 'self.on("click", function () {' +
+                    'var selText = window.getSelection().toString(); ' +
+                    'console.log("selText: " + selText); ' +
+                    'self.postMessage(selText); ' +
+                  '});',
+                    
   onMessage: function(word) {
-      word = word.charAt(0).toUpperCase() + word.subString(1,word.length).toLowerCase();
+      word = word.charAt(0).toUpperCase() + word.substring(1,word.length).toLowerCase();
       console.log("send word: " + word);
-      self.port.emit("getWord", word);
       cntntPnl.show();
-     } 
+      cntntPnl.port.emit("getWord", word);
+     }
 });
 
